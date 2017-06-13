8e9fd5806a44ed1535502d08fd60346d85a8a4e9
ryanjsalva <ryanjsalva@gmail.com>
2017-06-13 15:34:12 +0200

    first commit

---
+++ index.ios.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+export default class reactive extends Component {
+
+  // method to handle button press and pull a terrible joke!
+  state = { joke: '...' }
+  handlePress(e) {
+   this.setState({joke: '...'})
+   fetch('https://icanhazdadjoke.com/', { headers: { Accept: 'text/plain' } }).then(r => r.text())
+     .then(joke => this.setState({ joke }));
+  }
+
+  // record user engagement, analytics
+  trackEvent() {
+    Analytics.trackEvent('foobar', {'horse': 'brown'});
+  }
+
+  // render the component
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {this.state.joke}
+        </Text>
+        <Button title='I am not laughing' onPress={() => this.handlePress()} />
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('reactive', () => reactive);
